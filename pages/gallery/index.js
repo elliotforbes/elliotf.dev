96b093dd0d9d4d4fd89f33ec47131cea29973cce
Elliot Forbes <elliotforbes@Elliots-MBP-2.localdomain>
2022-06-06 09:16:33 +0100

    updates

--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -1,62 +1,54 @@
 import { useState, useEffect } from "react"
 import axios from 'axios';
 
-export default function Gallery({}) {
-    const [data, setData] = useState({
-        gallery: []
-    })
+export default function Gallery({ }) {
+  const [data, setData] = useState({
+    gallery: []
+  })
 
-    useEffect(() => {
-        axios.get("/api/gallery")
-            .then(resp => {
-                let gallery = resp?.data;
-                setData({
-                    gallery
-                });
-            })
-    }, [])
+  useEffect(() => {
+    axios.get("/api/gallery")
+      .then(resp => {
+        let gallery = resp?.data;
+        setData({
+          gallery
+        });
+      })
+  }, [])
 
-    return (
+  return (
+    <div>
+      <h2 className="text-xl font-extrabold mb-8">My Super Awesome Gallery</h2>
+      <div className="grid grid-cols-4">
         <div>
-            <h2 className="text-xl font-extrabold mb-8">My Super Awesome Gallery</h2>
-            <div className="grid grid-cols-4">
-                <div>
-                    {data.gallery?.images?.map((image) => (
-                        <div>
-                            <img src={image} alt={image} />
-                        </div>
-                    )).filter(function (value, index, ar) {
-                        return (index % 4 == 0);
-                    })}
-                </div>
-                <div>
-                {data.gallery?.images?.map((image) => (
-                    <div>
-                        <img src={image} alt={image} />
-                    </div>
-                )).filter(function (value, index, ar) {
-                    return (index % 4 == 1);
-                })}
-                </div>
-                <div>
-                {data.gallery?.images?.map((image) => (
-                    <div>
-                        <img src={image} alt={image} />
-                    </div>
-                )).filter(function (value, index, ar) {
-                    return (index % 4 == 2);
-                })}
-                </div>
-                <div>
-                {data.gallery?.images?.map((image) => (
-                    <div>
-                        <img src={image} alt={image} />
-                    </div>
-                )).filter(function (value, index, ar) {
-                    return (index % 4 == 3);
-                })}
-                </div>
-            </div>
+          {data.gallery?.images?.map((image) => (
+            <img key={image} src={image} alt={image} />
+          )).filter(function (value, index, ar) {
+            return (index % 4 == 0);
+          })}
         </div>
-    )
+        <div>
+          {data.gallery?.images?.map((image) => (
+            <img key={image} src={image} alt={image} />
+          )).filter(function (value, index, ar) {
+            return (index % 4 == 1);
+          })}
+        </div>
+        <div>
+          {data.gallery?.images?.map((image) => (
+            <img key={image} src={image} alt={image} />
+          )).filter(function (value, index, ar) {
+            return (index % 4 == 2);
+          })}
+        </div>
+        <div>
+          {data.gallery?.images?.map((image) => (
+            <img key={image} src={image} alt={image} />
+          )).filter(function (value, index, ar) {
+            return (index % 4 == 3);
+          })}
+        </div>
+      </div>
+    </div>
+  )
 }
