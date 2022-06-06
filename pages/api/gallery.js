229fe37e16e3b6a6d08b01b88dc065b7cd247add
Elliot Forbes <elliotforbes@Elliots-MBP-2.localdomain>
2022-06-06 09:13:00 +0100

    Added gallery

---
+++ pages/api/gallery.js
@@ -0,0 +1,21 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+    res.status(200).json({ 
+        images: [
+            'https://media.elliotf.dev/2022/06/03/DSC00413.jpg',
+            'https://media.elliotf.dev/2022/06/03/DSC00372.jpg',
+            'https://media.elliotf.dev/2022/06/03/DSC00435.jpg',
+            'https://media.elliotf.dev/2022/06/03/DSC00408.jpg',
+            'https://media.elliotf.dev/2022/06/03/DSC00437.jpg',
+            'https://media.elliotf.dev/2022/06/03/DSC00507.jpg',
+            'https://media.elliotf.dev/2022/06/03/DSC00502.jpg',
+            'https://media.elliotf.dev/2022/06/03/DSC00485.jpg',
+            'https://media.elliotf.dev/2022/06/03/DSC00492.jpg',
+            'https://media.elliotf.dev/2022/06/03/DSC00489.jpg',
+            'https://media.elliotf.dev/2022/06/03/DSC00510.jpg',
+            'https://media.elliotf.dev/2022/06/03/DSC00569.jpg'
+        ]
+    })
+  }
+  
---
+++ pages/gallery/index.js
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react"
+import axios from 'axios';
+
+export default function Gallery({}) {
+    const [data, setData] = useState({
+        gallery: []
+    })
+
+    useEffect(() => {
+        axios.get("/api/gallery")
+            .then(resp => {
+                let gallery = resp?.data;
+                setData({
+                    gallery
+                });
+            })
+    }, [])
+
+    return (
+        <div>
+            <h2 className="text-xl font-extrabold mb-8">My Super Awesome Gallery</h2>
+            <div className="grid grid-cols-4">
+                <div>
+                    {data.gallery?.images?.map((image) => (
+                        <div>
+                            <img src={image} alt={image} />
+                        </div>
+                    )).filter(function (value, index, ar) {
+                        return (index % 4 == 0);
+                    })}
+                </div>
+                <div>
+                {data.gallery?.images?.map((image) => (
+                    <div>
+                        <img src={image} alt={image} />
+                    </div>
+                )).filter(function (value, index, ar) {
+                    return (index % 4 == 1);
+                })}
+                </div>
+                <div>
+                {data.gallery?.images?.map((image) => (
+                    <div>
+                        <img src={image} alt={image} />
+                    </div>
+                )).filter(function (value, index, ar) {
+                    return (index % 4 == 2);
+                })}
+                </div>
+                <div>
+                {data.gallery?.images?.map((image) => (
+                    <div>
+                        <img src={image} alt={image} />
+                    </div>
+                )).filter(function (value, index, ar) {
+                    return (index % 4 == 3);
+                })}
+                </div>
+            </div>
+        </div>
+    )
+}
